aeb002f7eae22c968074c72d89789d422bc79789
Dbonejones <devonross@gmail.com>
2017-08-22 13:25:57 -0400

    customer drawings

---
+++ nautcustom/drawings_portal/doctype/customer_drawings/customer_drawings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Nautican and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Drawings', {
+	refresh: function(frm) {
+
+	}
+});
